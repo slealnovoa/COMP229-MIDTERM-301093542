f13ab0e50b485d22ec7da15f1a5db5384bff1894
Santiago Leal <slealnov@my.centennialcollege.ca>
2022-10-28 23:29:34 -0400

    added routes and delete/edit buttons

--- server/controllers/books.js
+++ server/controllers/books.js
@@ -16,38 +16,142 @@ export function displayBookList(req, res, next) {
 //  GET the Book Details page in order to add a new Book
 export function displayAddPage(req, res, next) {
 
-    /*****************
-    * ADD CODE HERE *
-    *****************/
+    router.get('/add', function(req, res) {
+
+        res.render('books/add.ejs', {
+      
+          title: 'Add Book',
+      
+          book: {},
+      
+        });
+      
+      });
 }
 
 // POST process the Book Details page and create a new Book - CREATE
 export function processAddPage(req, res, next) {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    router.post('/add', function(req, res) {
+
+        let book = new Book({
+      
+          title: req.body.title,
+      
+          author: req.body.author,
+      
+          genre: req.body.genre,
+      
+          read: req.body.read,
+      
+        });
+      
+        book.save(function(err) {
+      
+          if (err) {
+      
+            console.log(err);
+      
+            return;
+      
+          } else {
+      
+            res.redirect('/books');
+      
+          }
+      
+        });
+      
+      });
 }
 
 // GET the Book Details page in order to edit an existing Book
 export function displayEditPage(req, res, next) {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    router.get('/:id', function(req, res) {
+
+        let id = req.params.id;
+      
+        Book.findById(id, function(err, book) {
+      
+          if (err) {
+      
+            console.log(err);
+      
+            return;
+      
+          }
+      
+          res.render('books/details.ejs', {
+      
+            title: 'Edit Book',
+      
+            book: book,
+      
+          });
+      
+        });
+      
+      });
 
 }
 
 // POST - process the information passed from the details form and update the document
 export function processEditPage(req, res, next) {
-    /*****************
-    * ADD CODE HERE *
-    *****************/
+    router.post('/:id', function(req, res) {
+
+        let id = req.params.id;
+      
+        let book = new Book({
+      
+          _id: id,
+      
+          title: req.body.title,
+      
+          author: req.body.author,
+      
+          genre: req.body.genre,
+      
+          read: req.body.read,
+      
+        });
+      
+        Book.update({_id: id}, book, function(err) {
+      
+          if (err) {
+      
+            console.log(err);
+      
+            return;
+      
+          }
+      
+          res.redirect('/books');
+      
+        });
+      
+      });
 }
 
 // GET - process the delete by user id
 export function processDelete(req, res, next) {
-    /*****************
-  * ADD CODE HERE *
-  *****************/
+    router.get('/delete/:id', function(req, res) {
+
+        let id = req.params.id;
+      
+        Book.remove({_id: id}, function(err) {
+      
+          if (err) {
+      
+            console.log(err);
+      
+            return;
+      
+          }
+      
+          res.redirect('/books');
+      
+        });
+      
+      });
 }
